fceeef387ccd0e9ee80f0c802f4dc5077fd580f2
Mirco Giardina <mirco.giardina@ennova-research.com>
2023-12-15 15:11:14 +0100

    feat: projection challenge
    https://angular-challenges.vercel.app/challenges/angular/1-projection/

--- apps/angular/projection/src/app/component/teacher-card/teacher-card.component.ts
+++ apps/angular/projection/src/app/component/teacher-card/teacher-card.component.ts
@@ -1,35 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-import { FakeHttpService } from '../../data-access/fake-http.service';
+import { Component, OnInit, signal } from '@angular/core';
+import {
+  FakeHttpService,
+  randTeacher,
+} from '../../data-access/fake-http.service';
 import { TeacherStore } from '../../data-access/teacher.store';
-import { CardType } from '../../model/card.model';
 import { Teacher } from '../../model/teacher.model';
 import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-teacher-card',
-  template: `<app-card
-    [list]="teachers"
-    [type]="cardType"
-    customClass="bg-light-red"></app-card>`,
+  template: `
+    <app-card [list]="teachers()" (newItem)="newItem()" class="bg-light-red">
+      <img src="assets/img/teacher.png" width="200px" />
+      <ng-template #rowRef let-teacher>
+        <app-list-item (deleteOne)="deleteOne(teacher.id)">
+          {{ teacher.firstName }}
+        </app-list-item>
+      </ng-template>
+    </app-card>
+  `,
   styles: [
     `
-      ::ng-deep .bg-light-red {
+      .bg-light-red {
         background-color: rgba(250, 0, 0, 0.1);
       }
     `,
   ],
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, ListItemComponent],
 })
 export class TeacherCardComponent implements OnInit {
-  teachers: Teacher[] = [];
-  cardType = CardType.TEACHER;
+  // Example of signal use. Nb: teachers() transform the signal it's value, in this case an array
+  teachers = signal<Teacher[]>([]);
 
-  constructor(private http: FakeHttpService, private store: TeacherStore) {}
+  constructor(
+    private http: FakeHttpService,
+    private store: TeacherStore,
+  ) {}
 
   ngOnInit(): void {
     this.http.fetchTeachers$.subscribe((t) => this.store.addAll(t));
 
-    this.store.teachers$.subscribe((t) => (this.teachers = t));
+    this.store.teachers$.subscribe((t) => this.teachers.update(() => t));
+  }
+
+  newItem() {
+    this.store.addOne(randTeacher());
+  }
+
+  deleteOne(id: number) {
+    this.store.deleteOne(id);
   }
 }
